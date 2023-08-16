c2c74029d0f6797e59332596bde7022114ffd9d5
mchau98 <nguynminhchau98@gmail.com>
2023-08-16 23:52:49 +0700

    first commit

---
+++ app/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+# Create your views here.
+def home(request):
+    products = Product.objects.all()
+    context= {'products': products}
+    return render(request,'app/home.html',context)
+def cart(request):
+    if request.user.is_authenticated: 
+        customer = request.user.customer
+        order, create = Order.objects.get_or_create(customer=customer,complete=False)
+        items = order.orderitems_set.all()
+    else:
+        items =[]
+        order = {'get_cart_items':0,'get_cart_total':0}
+    context= {'items':items,'order':order}
+    return render(request,'app/cart.html',context)
+def checkout(request):
+    if request.user.is_authenticated: 
+        customer = request.user.customer
+        order, create = Order.objects.get_or_create(customer=customer,complete=False)
+        items = order.orderitems_set.all()
+    else:
+        items =[]
+        order = {'get_cart_items':0,'get_cart_total':0}
+    context= {'items':items,'order':order}
+    return render(request,'app/checkout.html',context)
